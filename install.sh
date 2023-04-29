18938ad8f08492218f320c98b0f653dbc1395694
nltb99 <nguyenletienbao99@gmail.com>
2023-04-29 11:21:11 +0700

    feat: add symbolic links by OS

--- install.sh
+++ install.sh
@@ -1,3 +1,25 @@
 # Link neovim
 ln -s  $(pwd)/nvim ~/.config
 
+# Link zsh
+ln -s  $(pwd)/.zshrc ~/
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+    # Link alacritty config
+   ln -s  $(pwd)/alacritty.yml ~/.config
+
+    # Link tmux config
+   ln -s  $(pwd)/.tmux.conf ~/
+
+    # Link vscode settings
+   ln -s  $(pwd)/settings.json ~/Library/Application\ Support/Code/User
+   ln -s $(pwd)/keybindings.json ~/Library/Application\ Support/Code/User
+fi
+
+if [[ "$OSTYPE" =~ ^linux ]]; then
+   # Linux
+fi
+
+if [[ "$OSTYPE" =~ ^msys ]]; then
+   # Windows
+fi
