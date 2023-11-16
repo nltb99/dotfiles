3fe03fdb169a38f9efdc381bdd1da6f4d3675453
nltb99 <nguyenletienbao99@gmail.com>
2023-11-17 00:03:50 +0700

    fix: update folder nvim

--- nvim_v2/lua/user/copilot.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, project = pcall(require, "copilot")
-if not status_ok then
-	return
-end
-
---[[ vim.g.copilot_no_tab_map = true ]]
---[[ vim.g.copilot_assume_mapped = true ]]
---[[ vim.g.copilot_tab_fallback = "" ]]
