6581a69bc8395817374128d2339d3db24a6663ba
nltb99 <nguyenletienbao99@gmail.com>
2023-11-18 10:17:22 +0700

    feat: update colorscheme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,13 +1,8 @@
 vim.cmd([[
 try
-  " colorscheme darkplus
-  " colorscheme nightfox
-  " colorscheme kanagawa
-  " colorscheme tokyonight-storm
-  " colorscheme tokyonight-day
-  colorscheme tokyonight-moon
+   colorscheme onedark
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
-  set background=dark
+  -- set background=dark
 endtry
 ]])
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -72,7 +72,7 @@ return packer.startup(function(use)
 
 	-- Colorschemes
 	-- Please set Colorschemes in colorscheme.lua
-	use({ "folke/tokyonight.nvim" })
+	use 'navarasu/onedark.nvim'
 	-- use({ "EdenEast/nightfox.nvim", tag = "v1.0.0" })
 	-- use({"rebelot/kanagawa.nvim", commit = "5f1203f4606e3208f6dd156c03f498d9fadcf1a8"})
 	--[[ use({ "lunarvim/darkplus.nvim", commit = "93fb1fd7b2635192d909e11a77256d5822aed5c8" }) ]]
