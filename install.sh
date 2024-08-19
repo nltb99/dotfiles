acf0542be16064c47eb8e5e81fdecd141153264c
nltb99 <nguyenletienbao99@gmail.com>
2024-08-19 21:18:22 +0700

    feat: add skhd configs

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 # MacOS & Linux
 if [[ "$OSTYPE" =~ ^darwin || "$OSTYPE" =~ ^linux ]]; then
-   echo 'Linking "./nvim" to "~/.config"'
+   echo 'Linking nvim'
    ln -s  $(pwd)/.config/nvim ~/.config
 
-   echo 'Linking "./zshrc" to "~/"'
-   ln -s  $(pwd)/.config/.zshrc ~/
-
-   echo 'Linking "./alacritty.yml" to "~/.config"'
+   echo 'Linking alacritty.yml'
    ln -s  $(pwd)/.config/alacritty.yml ~/.config
 
-   echo 'Linking ".tmux.conf" to "~/"'
+   echo 'Linking .config/skhd'
+   ln -s  $(pwd)/.config/skhd ~/.config
+
+   echo 'Linking .zshrc'
+   ln -s  $(pwd)/.config/.zshrc ~/
+
+   echo 'Linking .tmux.conf'
    ln -s  $(pwd)/.config/.tmux.conf ~/
 
    echo 'Linking vscode settings'
    ln -s  $(pwd)/.config/settings.json ~/Library/Application\ Support/Code/User
    ln -s $(pwd)/.config/keybindings.json ~/Library/Application\ Support/Code/User
 
-   echo 'Linking ".tmux.conf" to "~/"'
-   ln -s  $(pwd)/.config/.tmux.conf ~/
 fi
 
 # TODO: Will update this after experienced setup with windows
