daabdb4588e1c050c31fe69cdf003dba697de28c
nltb99 <nguyenletienbao99@gmail.com>
2025-03-19 20:49:03 +0700

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,27 @@
 # MacOS & Linux
 if [[ "$OSTYPE" =~ ^darwin || "$OSTYPE" =~ ^linux ]]; then
-   ## ~/.config
-   echo 'Linking nvim'
-   ln -s  $(pwd)/.config/nvim ~/.config
+   ## Remove Symlinks Inside .config (But Keep the Directory) 
+   find ~/.config -type l -exec rm {} +
 
-   echo 'Linking alacritty.yml'
-   # ln -s  $(pwd)/.config/alacritty.yml ~/.config
-   ln -s  $(pwd)/.config/alacritty.toml ~/.config
+   ## Create Symlinks for Each File/Folder Inside .config
+   ln -sfn $(pwd)/.config/* ~/.config/
 
-   echo 'Linking yabai'
-   ln -s  $(pwd)/.config/yabai ~/.config/yabai
+   # ## ~/.config
+   # echo 'Linking nvim'
+   # ln -s  $(pwd)/.config/nvim ~/.config
 
-   echo 'Linking skhd'
-   ln -s  $(pwd)/.config/skhd ~/.config/skhd
+   # echo 'Linking alacritty.yml'
+   # # ln -s  $(pwd)/.config/alacritty.yml ~/.config
+   # ln -s  $(pwd)/.config/alacritty.toml ~/.config
 
-   echo 'Linking Karabiner'
-   ln -s  $(pwd)/.config/karabiner ~/.config/karabiner
+   # echo 'Linking yabai'
+   # ln -s  $(pwd)/.config/yabai ~/.config/yabai
+
+   # echo 'Linking skhd'
+   # ln -s  $(pwd)/.config/skhd ~/.config/skhd
+
+   # echo 'Linking Karabiner'
+   # ln -s  $(pwd)/.config/karabiner ~/.config/karabiner
 
    ## root
    echo 'Linking qutebrowser'
@@ -26,10 +32,6 @@ if [[ "$OSTYPE" =~ ^darwin || "$OSTYPE" =~ ^linux ]]; then
 
    echo 'Linking .tmux.conf'
    ln -s  $(pwd)/.tmux.conf ~/
-
-   echo 'Linking vscode settings'
-   ln -s ~/Library/Application\ Support/Code/User/settings.json $(pwd)/.config/vscode/settings.json
-   ln -s ~/Library/Application\ Support/Code/User/keybindings.json $(pwd)/.config/vscode/keybindings.json
 fi
 
 # TODO: Will update this after experiencing setup with Windows.
